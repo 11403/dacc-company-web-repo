4d60a8a953b64495afcee9feabd2036dbd3466cc
Johnson <23JohnsonS@mydacc.org>
2021-09-10 10:19:22 -0400

    finished with my website
    version 1

--- great_connector/script/javascript/script.js
+++ great_connector/script/javascript/script.js
@@ -2,13 +2,21 @@
 
 const nightLight = document.querySelector(".nightlight"); 
 const zoom = document.querySelector(".zoom");
-const body = document.querySelector("*"); 
+const body = document.querySelector("html"); 
+const easterEgg = document.querySelector("#easter_egg"); 
 //let nightLightOn = false; 
 
 nightLight.addEventListener("click", function () { 
     //console.log("working"); 
     //nightLightOn ? body.classList.remove("invert") : body.classList.add("invert"); 
     body.classList.toggle("invert"); 
+    if (body.classList.contains("invert")) { 
+    //console.log("On") 
+    easterEgg.style.opacity = "1"; 
+} else { 
+        //console.log("Off")
+        easterEgg.style.opacity = "0";
+    }  
 })
 
 zoom.addEventListener("click", function () { 
@@ -16,3 +24,4 @@ zoom.addEventListener("click", function () {
 })
 
 //!0 ? console.log("false") : console.log("true"); 
+
