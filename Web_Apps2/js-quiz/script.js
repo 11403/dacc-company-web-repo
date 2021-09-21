a0f01ad9ac0af4e05544a486a1c947554afeafd9
Johnson <23JohnsonS@mydacc.org>
2021-09-21 07:58:29 -0400

    sure

---
+++ Web_Apps2/js-quiz/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// 1.) it won't work because you used href instead of src="" 
+
+// 2.)      1. var      2. Const        3. let
+
+// 3.) when clicked, the name input box will have its value set to "AppDev1 Student"
+
+// 4.) document.getElementById("my_submit").innerHTML = text; 
+
